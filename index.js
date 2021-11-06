f7678912ddcdc106dd8e50b5c733df0890c35bdf
Bryan Gabe <46936654+BryanGabe00@users.noreply.github.com>
2021-11-06 12:31:29 -0500

    Final project proposal

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const express = require('express')
-const expressHandlebars = require('express-handlebars')
-
-const app = express()
-
-app.engine('handlebars', expressHandlebars({
-    defaultLayout: 'main',
-}))
-app.use(express.static(__dirname + '/public'))
-app.set('view engine', 'handlebars')
-
-const port = process.env.port || 3000
+var http = require('http');
+var url = require('url');
+var dt = require('./datetime');
 
 
-app.get('/', (req, res) => { res.render('index') })
-app.listen(port, () => {
-    console.log(`Server started listening on port: ${port}`)
-})
+const server = http.createServer((request, response) => {
+    // Write the request to the log. 
+    console.log(request);
+
+    // Standard Hello World.
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write('<h3>Hello World!</h3>')
+
+    // Access funcion from a separate JavaScript module.
+    response.write("The date and time are currently: " + dt.myDateTime() + "<br><br>");
+
+    // Show the url. 
+    response.write("req.url="+request.url+"<br><br>");
+
+    // Suggest adding something tl the url so that we can parse it. 
+    response.write("Consider adding '/test?year=2017&month=July' to the URL.<br><br>");
+    var q = url.parse(request.url, true).query;
+    var txt = q.year + " " + q.month;
+    response.write("txt="+txt);
+
+    // Close the response
+    response.end('<h3>The End.</h3>');
+});
+
+const port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
